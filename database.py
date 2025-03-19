a893d752a96370684c70f8d00e783a7dfa008287
baxtiyor bekmurodov <baxtiyorbekmurodov@baxtiyors-MacBook-Air.local>
2025-03-19 10:04:05 +0500

    Auth

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_USERNAME = 'postgres'
-DATABASE_PASSWORD = 'baxtiorgreat'
+DATABASE_USERNAME = 'username'
+DATABASE_PASSWORD = 'password'
 DATABASE_HOST = 'localhost'
 DATABASE_PORT = '5432'
-DATABASE_NAME = 'users'
+DATABASE_NAME = 'databasename'
 
 DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 
